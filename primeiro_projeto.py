5912f7cfb51107d1cf4073a2fe0c8f19689cb8fe
IrailtonSilvaDev <irailtonssb@gmail.com>
2025-08-20 21:19:39 -0300

    Add files via upload

---
+++ primeiro_projeto.py
@@ -0,0 +1,19 @@
+
+nume1=int(input("insira o primeiro numero:"))
+nume2=int(input("insira o segudo numero:"))
+
+soma=nume1+nume2
+subtracao=nume1-nume2
+mutiplicacao=nume1*nume2
+divisao=nume1/nume2
+exponenciacao=nume1**nume2
+resto_da_divisao=nume1%nume2
+divisao_inteira=nume1//nume2
+
+print("a soma e:",soma)
+print("a subtracao e:",subtracao)
+print("a divisao e:",divisao)
+print("a exponenciacao:",exponenciacao)
+print("o resto_da_divisao:",resto_da_divisao)
+print("divisao_inteiro:",divisao_inteira)
+
